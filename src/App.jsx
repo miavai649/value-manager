7d96679953964ee80f8db69cac9f0cf54d7e236c
miavai649 <mahmudulhaquenoor@gmail.com>
2026-08-05 07:26:45 +0600

    feat: add SearchAndSorting component and integrate it into App

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,23 @@
 import Footer from './components/layout/Footer';
 import Header from './components/layout/Header';
 import NewBookmark from './components/NewBookmark';
+import SearchAndSorting from './components/SearchAndSorting';
 
 function App() {
   return (
     <>
       <Header />
       <NewBookmark />
+
+      {/* main content */}
+      <main class='p-8'>
+        <div class='max-w-7xl mx-auto space-y-10 px-4'>
+          {/*    <!-- Search, Sort, and Filter Buttons --> */}
+          <SearchAndSorting />
+
+          {/* <!-- Password Cards Grid --> */}
+        </div>
+      </main>
       <Footer />
     </>
   );
